91831ffbf6681cf731d7f29bfd00f63790ec2786
SindujaDonapati <sindujadonapati@gmail.com>
2025-03-26 17:05:36 +0530

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,124 @@
+# # 1 to 100 all numbers
+
+# for i in range(1,101):
+#     print(i)
+
+
+# # factorial of a number
+
+# n = int(input("Enter any number: "))
+# f = 1
+# while n>=1:
+#     f *= n
+#     n -= 1
+# print("Factorial i",f)
+
+
+# # sum of first n natural numbers
+
+# num=int(input("enter any number:"))
+# sum=0
+# sum=int(num*(num+1)/2)
+# print(sum)
+
+
+# # using while loop print even numbers from 1 to 50
+# n=int(input("enter the value:"))
+# i=1
+# while i<n:
+#     if i % 2 == 0:
+#         print(i)
+#     i+=1
+
+
+# # multiplication table of a given numbers
+
+# for i in range(1,21):
+#     for j in range(1,11):
+#         print(i,"*",j,"=",i*j)
+
+
+# # reverse a number
+
+# reverse=int(input("enter the number:"))
+# rev_num=0
+# sum=0
+# count=0
+# while reverse>0:
+#     rem = reverse % 10
+#     sum+=rem
+#     count+=1
+#     print(rem,end="")
+#     rev_num=rev_num*10+rem
+#     reverse = reverse // 10
+# print(rev_num)
+# print(sum)
+# print(count)
+
+
+# # enter numbers until they enter neagative number
+# while True:
+#     num=int(input("enter a number:"))
+#     if num<0:
+#        print("Negative")
+#        break
+
+
+
+# Medium
+
+
+# fabonacci series
+# num1 = 0
+# num2 = 1
+# # sum=0
+# for i in range(1,11):
+#     print(num1)
+#     num1,num2=num2,num1+num2
+
+
+# given number is prime or not
+
+# n=12
+# temp=True
+# for i in (2,n+1):
+#     if n%i==0:
+#         print("not a prime")
+#         break
+#     else:
+
+#          print("prime")
+
+
+# 15 
+
+# for i in range(1,101):
+#    if i % 15 == 0:
+#     print(i)
+
+# print("1.square")
+# print("2.cube")
+# print("3.exit")
+# choose=int(input("Enter a number:"))
+# if choose==1:
+#     num=int(input("Enter a number:"))
+#     print(num**2)
+# elif choose==2:
+#      num=int(input("Enter a number:"))
+#      print(num**3)
+# elif choose==3:
+#      print("Exit")
+# else:
+#      print("NUN")
+       
+    
+
+
+
+
+# 
+
+
+
+
+
